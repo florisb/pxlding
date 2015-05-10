83a154e742483ad9031393ee83b2ef8480893576
Coen Zimmerman <coen@pixelindustries.com>
2015-05-10 12:31:21 +0200

    include controller name in view

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -1,9 +1,9 @@
 <?php
 	namespace Controller;
-	
+
 	use PXL\Hornet\Controller\Controller;
 	use App;
-	
+
 	class BaseController extends Controller {
 
 		public function preAction() {
@@ -12,10 +12,12 @@
 			$this->set('isMobileTablet', $detect->isMobile() || $detect->isTablet(), true);
 			$this->set('isMobile', $detect->isMobile() && !$detect->isTablet(), true);
 			$this->set('isTablet', $detect->isTablet(), true);
+
+			$this->set('controllerName', $this->getControllerName(), true);
 		}
-		
+
 		public function postAction() {
-			
+
 		}
 
 	}
