d59cce6767ee4f3b99e6c8d7c9dc533e8ec806b9
Coen Zimmerman <coen@pixelindustries.com>
2015-05-14 11:31:21 +0200

    basecontroller cleanup

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -1,23 +1,26 @@
-<?php
-	namespace Controller;
+<?php namespace Controller;
 
-	use PXL\Hornet\Controller\Controller;
-	use App;
+use PXL\Hornet\Controller\Controller;
+use App;
 
-	class BaseController extends Controller {
+class BaseController extends Controller
+{
 
-		public function preAction() {
-			$detect = new App\Mobile_Detect;
+	public function preAction()
+	{
+		$detect = new App\Mobile_Detect;
 
-			$this->set('isMobileTablet', $detect->isMobile() || $detect->isTablet(), true);
-			$this->set('isMobile', $detect->isMobile() && !$detect->isTablet(), true);
-			$this->set('isTablet', $detect->isTablet(), true);
+		$this->set('isMobileTablet', $detect->isMobile() || $detect->isTablet(), true);
+		$this->set('isMobile', $detect->isMobile() && !$detect->isTablet(), true);
+		$this->set('isTablet', $detect->isTablet(), true);
 
-			$this->set('controllerName', $this->getControllerName(), true);
-		}
+		$this->set('controllerName', $this->getControllerName(), true);
+	}
 
-		public function postAction() {
+	public function postAction()
+	{
 
-		}
 
 	}
+
+}
