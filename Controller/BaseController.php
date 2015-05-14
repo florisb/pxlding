840a6fa33a6efe01b925224ce99fc0bfc3f1a8c0
Coen Zimmerman <coen@pixelindustries.com>
2015-05-14 11:31:29 +0200

    basecontroller: added menu items

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -20,7 +20,50 @@ class BaseController extends Controller
 	public function postAction()
 	{
 
+		// header content
+		$headerMenu = array(
+			(object) array(
+				'title'  => 'Pixelindustries',
+				'url'    => $this->route(''),
+				'active' => ($this->getControllerName() == 'home'),
+			),
+			(object) array(
+				'title'  => 'Our mission',
+				'url'    => $this->route('mission'),
+				'active' => ($this->getControllerName() == 'mission'),
+			),
+			(object) array(
+				'title'  => 'Services',
+				'url'    => $this->route('services'),
+				'active' => ($this->getControllerName() == 'services'),
+			),
+			(object) array(
+				'title'  => 'Cases',
+				'url'    => $this->route('cases'),
+				'active' => ($this->getControllerName() == 'cases'),
+			),
+			(object) array(
+				'title'  => 'Blog',
+				'url'    => $this->route('blog'),
+				'active' => ($this->getControllerName() == 'blog'),
+			),
+			(object) array(
+				'title'  => 'Careers',
+				'url'    => $this->route('jobs'),
+				'active' => ($this->getControllerName() == 'jobs'),
+			),
+			(object) array(
+				'title'  => 'Contact',
+				'url'    => $this->route('contact'),
+				'active' => ($this->getControllerName() == 'contact'),
+			),
+		);
 
+		// footer content
+		$footerMenu = $headerMenu;
+
+		$this->set('headerMenu', $headerMenu, true);
+		$this->set('footerMenu', $footerMenu, true);
 	}
 
 }
