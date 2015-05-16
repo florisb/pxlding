efe7092869e75937fd4dfb17b006fd965d82154f
Coen Zimmerman <coen@pixelindustries.com>
2015-05-17 00:53:47 +0200

    mission controller added

---
+++ Controller/mission.php
@@ -0,0 +1,20 @@
+<?php
+	namespace Controller;
+
+	use Model\Factory;
+
+	class Mission extends BaseController {
+
+		public function indexAction() {
+
+			$missionContent = Factory\Content::getSettingsForPage('mission');
+			$peopleContent  = Factory\Content::getSettingsForPage('people');
+			$employees      = Factory\Employees::getAll(true);
+
+			$this->set('missionContent', $missionContent);
+			$this->set('peopleContent',  $peopleContent);
+			$this->set('employees',      $employees);
+
+		}
+
+	}
