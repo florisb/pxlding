c7f020eff15dcb1fa7eb0781a7bca2fe94f74d01
Coen Zimmerman <coen@pixelindustries.com>
2015-05-17 18:01:32 +0200

    blog basics added

---
+++ Controller/blog.php
@@ -0,0 +1,30 @@
+<?php
+	namespace Controller;
+
+	use Model\Factory;
+
+	class Blog extends BaseController {
+
+		public function indexAction($page = null) {
+
+			if (empty($page)) {
+				$page = 1;
+			}
+
+			$blog = Factory\Blog::getAll($page);
+
+			$this->set('blog', $blog);
+		}
+
+
+		public function detailsAction() {
+
+			$slug     = $this->getParam('slugparam');
+
+			$blogpost = Factory\Blog::getBySlug($slug);
+			$gallery  = Factory\Cases::getGalleryById($blogpost->id);
+
+			$this->set('blogpost', $blogpost);
+			$this->set('gallery',  $gallery);
+		}
+	}
