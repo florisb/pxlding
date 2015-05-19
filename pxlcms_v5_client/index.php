af41b119fce8f219e6c9dea622647aa656af87c6
Coen Zimmerman <coen@pixelindustries.com>
2015-05-19 10:51:31 +0200

    whitespace cleanup

--- pxlcms_v5_client/index.php
+++ pxlcms_v5_client/index.php
@@ -1,12 +1,12 @@
 <?php
 	$_SERVER['AJAX_CALL'] = false;
 	include "meta.php";
-	
+
 	// use output buffering to allow in-process-redirection
 	ob_start();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" id='html'> 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" id='html'>
 	<head>
 		<title>CMS: <?= $CMS_ENV['pxlcms_client'] ?></title>
 		<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE8" />
@@ -18,7 +18,7 @@
 		<meta name="robot" content="noindex,nofollow">
 		<meta name="author" content="Pixelindustries">
 		<meta name="language" content="nl-nl">
-		
+
 		<?php
 			if (file_exists('config/favicon.png')) {
 				echo "<link rel='shortcut icon' href='config/favicon.png' />";
@@ -32,7 +32,7 @@
 		<!--[if lt IE 7]>
 			<link rel='stylesheet' title='IE_Only' href='includes/ie6_fixes.css' />
 		<![endif]-->
-		
+
 		<script type="text/javascript" src="includes/scriptaculous/prototype.js"></script>
 		<script type="text/javascript" src="includes/default.js"></script>
 		<script type="text/javascript" src="includes/dragresize.js"></script>
@@ -45,13 +45,13 @@
 		<script type="text/javascript" src="includes/swfupload/upload_handler.js"></script>
 		<script type="text/javascript" src="includes/slick.js"></script>
 		<script type="text/javascript" src="includes/slider.js"></script>
-		
+
 		<script type="text/javascript" src="includes/pxlcms.js"></script>
 		<script type="text/javascript" src="includes/pxlcms.locationfield.js"></script>
-		
+
 		<script type="text/javascript" src="//maps.googleapis.com/maps/api/js?key=<?php echo($CMS_EXTRA['google_api_key']); ?>&sensor=false&language=nl"></script>
 		<script type="text/javascript" src="//www.google.com/jsapi"></script>
-		
+
 		<?php
 			// find and load custom module JS files
 			$CMS = new CMS();
@@ -67,9 +67,9 @@
 				}
 			}
 		?>
-		
+
 	</head>
-	
+
 	<body style='<?php
 			if ($_SERVER['HTTP_HOST'] == 'www2.pixelindustries.com' && file_exists('config/header_local.jpg')) {
 				echo "background-image: url(config/header_local.jpg);";
@@ -78,18 +78,18 @@
 			}
 		?>' onload="keep_alive_timer();" onunload="">
 		<div id='topanchor' style='position: absolute;'></div>
-		
+
 		<div id='help' style='display: none;'></div>
 		<div id='dark' style='display: none;'></div>
-	
+
 		<!-- MAIN CONTENTS -->
 		<div id='loading_indicator' style='display: none;'>Loading</div>
-		
+
 		<div id='topmenu'>
 			<a href='login/logout.php' onmouseover="new Effect.Opacity(this, { from: 0.25, to: 0.9, duration: 0.1 });" onmouseout="new Effect.Opacity(this, { from: 0.9, to: 0.25, duration: 0.3 });" style='background: url(img/topmenu/logout.gif); width: 43px;'></a>
 			<?php
 				// echo "<img style='cursor: pointer;' src='img/topmenu/help.gif' onclick=\"show_help();\" onmouseover=\"new Effect.Opacity(this, { from: 0.25, to: 0.9, duration: 0.1 });\" onmouseout=\"new Effect.Opacity(this, { from: 0.9, to: 0.25, duration: 0.3 });\" />";
-				
+
 				if ($CMS_SESSION['super_admin']) {
 					?>
 						<img style='cursor: pointer;' src='img/topmenu/logs.gif' onclick="process_setting({special_page: 'logs'});" onmouseover="new Effect.Opacity(this, { from: 0.25, to: 0.9, duration: 0.1 });" onmouseout="new Effect.Opacity(this, { from: 0.9, to: 0.25, duration: 0.3 });" />
@@ -99,29 +99,29 @@
 						<img src='img/topmenu/database.gif' onclick="process_setting({ special_page: 'database' });" onmouseover="new Effect.Opacity(this, { from: 0.25, to: 0.9, duration: 0.1 });" onmouseout="new Effect.Opacity(this, { from: 0.9, to: 0.25, duration: 0.3 });" />
 					<?php
 				}
-				
+
 				if ($CMS_SESSION['user']['user_manager']) {
 					?>
 						<img style='cursor: pointer;' src='img/topmenu/users.gif' onclick="process_setting({special_page: 'users'});" onmouseover="new Effect.Opacity(this, { from: 0.25, to: 0.9, duration: 0.1 });" onmouseout="new Effect.Opacity(this, { from: 0.9, to: 0.25, duration: 0.3 });" />
 					<?php
 				}
-				
+
 				if (isset($CMS_DB_PUBLISHED['db_name'])) {
 					?>
 						<img style='cursor: pointer;' src='img/topmenu/publish.gif' onclick="process_setting({special_page: 'publish'});" onmouseover="new Effect.Opacity(this, { from: 0.25, to: 0.9, duration: 0.1 });" onmouseout="new Effect.Opacity(this, { from: 0.9, to: 0.25, duration: 0.3 });" />
 					<?php
 				}
-				
+
 				if (isset($CMS_EXTRA['pxl_news_enabled']) && $CMS_EXTRA['pxl_news_enabled']) {
 					?>
 						<img style='cursor: pointer;' src='img/topmenu/news.gif' onclick="process_setting({special_page: 'news'});" onmouseover="new Effect.Opacity(this, { from: 0.25, to: 0.9, duration: 0.1 });" onmouseout="new Effect.Opacity(this, { from: 0.9, to: 0.25, duration: 0.3 });" />
 					<?php
 				}
-				
+
 				if (isset($CMS_EXTRA['backups_enabled']) && $CMS_EXTRA['backups_enabled']) {
 					?>
 						<img style='cursor: pointer;' src='img/topmenu/database.gif' onclick="process_setting({special_page: 'backups'});" onmouseover="new Effect.Opacity(this, { from: 0.25, to: 0.9, duration: 0.1 });" onmouseout="new Effect.Opacity(this, { from: 0.9, to: 0.25, duration: 0.3 });" />
-					<?php 
+					<?php
 				}
 
 				$CMS = new CMS();
@@ -132,7 +132,7 @@
 					}
 				}
 				$menu = array_reverse($menu);
-				
+
 				foreach ($menu as $m) {
 					?>
 					<img style='cursor: pointer;' src='img/topmenu/<?= $m['icon'] ?>' onclick="process_setting({cms_state: 'welcome', menu_id: <?= $m['id'] ?>});" onmouseover="new Effect.Opacity(this, { from: 0.25, to: 0.9, duration: 0.1 });" onmouseout="new Effect.Opacity(this, { from: 0.9, to: 0.25, duration: 0.3 });" />
@@ -140,17 +140,17 @@
 				}
 			?>
 		</div>
-		
+
 		<div id="mainframe">
 			<table border='0' cellspacing='0' cellpadding='0' style='width: 100%;'>
 				<tr valign='top'>
-					
+
 					<!-- LEFT COLUMN -->
 					<td style='width: <?= isset($CMS_EXTRA['menu_width']) ? $CMS_EXTRA['menu_width'] : 200 ?>px;' id='navigation'>
 						<?php
 							$CMS = new CMS();
 							$groups = $CMS->getStructure();
-							
+
 							// filter groups based on menu
 							$g = array();
 							foreach ($groups as $group) {
@@ -158,12 +158,12 @@
 								$g[] = $group;
 							}
 							$groups = $g;
-							
+
 							if (!isset($CMS_SESSION['group_id']) || !$CMS_SESSION['group_id']) {
 								$CMS_SESSION['group_id'] = array_peek($groups);
 								$CMS_SESSION['group_id'] = $CMS_SESSION['group_id']['id'];
 							}
-							
+
 							if (count($groups) > 1) {
 								$widget = new Widget();
 								$widget->height = '10px';
@@ -174,22 +174,22 @@
 								echo "</div>";
 								$widget->stop();
 							}
-							
+
 							$widget = new Widget();
 							$widget->height = '200px';
-							
+
 							foreach ($groups as $group) {
 								if ($group['id'] == $CMS_SESSION['group_id']) {
 									$widget->title = "<span id='group_nav_title'>".$group['name']."</span>";
 								}
-							}	
-							
+							}
+
 							$widget->start();
 							echo "<div style='' id='content_navigation'>";
 							include "core/navigation.php";
 							echo "</div>";
 							$widget->stop();
-							
+
 							$languages = $CMS->my_languages($CMS_SESSION['user']['id']);
 							if (count($languages) > 1)
 							{
@@ -205,10 +205,10 @@
 							}
 						?>
 					</td>
-					
+
 					<!-- spacer -->
 					<td style='width: 30px;'>&nbsp;</td>
-					
+
 					<!-- RIGHT COLUMN -->
 					<td>
 						<?php
@@ -220,7 +220,7 @@
 				</tr>
 			</table>
 		</div>
-		
+
 		<!-- FOOTER -->
 		<div id='footer'>
 			<?php
@@ -230,10 +230,10 @@
 				echo "</span>";
 			?>
 		</div>
-	
+
 	<?php
 		// echo "<div style='color: #fff;'>";pr($CMS_SESSION);echo "</div>";
 	?>
-	
+
 	</body>
 </html>
