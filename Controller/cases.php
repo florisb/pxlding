ed545509cb75fa0cafcdee9525679e3803f54160
Coen Zimmerman <coen@pixelindustries.com>
2015-05-23 01:25:38 +0200

    mock content for cases testing

--- Controller/cases.php
+++ Controller/cases.php
@@ -22,10 +22,16 @@
 
 			$case     = Factory\Cases::getBySlug($slug);
 			$gallery  = Factory\Cases::getGalleryById($case->cid);
-			// $cases    = Factory\Cases::getRandom();
 
-			// $this->set('cases', $cases);
-			$this->set('case', $case);
+			// until we got the generator fixed, insert mock stuff
+			$case->text_section_1 = "<h2>testing something</h2><p>text</p>";
+			$case->text_section_2 = "<h2>testing something</h2><p>text</p>";
+			$case->text_section_3 = "  &nbsp; fdgdfg";
+			$case->text_section_4 = "<h2>testing something</h2><p>text</p>";
+			$case->text_section_5 = "<h2>testing something END</h2><p>text</p>";
+
+
+			$this->set('case',    $case);
 			$this->set('gallery', $gallery);
 		}
 	}
