b2510d1be48041206730121356e571c2482b5310
Coen Zimmerman <coen@pixelindustries.com>
2015-05-23 23:54:51 +0200

    newsletter for jobs controller/view

--- Controller/home.php
+++ Controller/home.php
@@ -186,7 +186,95 @@ class Home extends BaseController
 
             $this->set('response', $response);
         }
+    }
+
+    /**
+     * Special newsletter signup for jobs only
+     */
+    public function newsletterjobsAction() {
+
+        if (isset($_POST['contact'])) {
+
+            $msg      = '';
+            $empty    = true;
+            $response = array();
+
+            foreach ($_POST as $key => $value) {
+
+                switch ($key) {
+
+                    // overhead
+                    case 'contact':
+                    case 'ajax':
+                        continue 2;
 
 
+                    case 'email':
+                        if (filter_var($value, FILTER_VALIDATE_EMAIL) == true) {
+                            $empty = false;
+                        } else {
+                            $response['errors'][$key] = true;
+                        }
+                        break;
+
+                    case 'message':
+                        if ( ! empty($value)) {
+                            $empty = false;
+                        } else {
+                            $response['errors'][$key] = true;
+                        }
+                        break;
+                }
+
+                if ( ! empty($value)) {
+                    $empty = false;
+                    $msg  .= "<strong>" . ucfirst(str_replace("_", " ", $key)) . "</strong>:<br/>" . $value . "<br/><br/>";
+                }
+
+
+                if (empty($value)) {
+                    $response['errors'][$key] = true;  //ML::label('contact form error empty ' . $key);
+                    //pr($response['errors']);
+                }
+
+            }
+
+            if ( ! count($response['errors'])) {
+
+                /*
+                 * to do: enable this newsletter.. Hello Dialog?
+                 *
+                 * The commented out stuff here has NOT been updated for
+                 * any sort of 'jobs only' newsletter!
+                 */
+
+                //     $contact = (Object) null;
+
+                //     $contact->email    = (string) pxl_db_safe($_POST['email']);
+                //     $contact->voornaam = (string) pxl_db_safe($_POST['name']);
+
+                //     $ch = curl_init();
+
+                //     curl_setopt($ch, CURLOPT_URL,
+                //         "https://app.klantenbinder2.nl/api/contacts?token=265b71d0f3466efa1566591a24927194");
+                //     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+                //     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($contact));
+                //     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+                //     $responseKb = curl_exec($ch);
+
+                //     curl_close($ch);
+
+
+                $response['result'] = true;
+
+            } else {
+                $response['result'] = false;
+            }
+
+            $this->set('response', $response);
+        }
 	}
+
 }
